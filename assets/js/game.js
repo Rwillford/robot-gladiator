aa7e6af3f3675b14770d9a5b7de500a0216019fb
Bob <rwillford0719@gmail.com>
2022-03-13 18:40:53 -0500

    1 million commits

--- assets/js/game.js
+++ assets/js/game.js
@@ -9,9 +9,8 @@ var enemyAttack = 12;
 
 // fight function
 var fight = function(enemyName) {
-    // Alert players that they are starting the round
-    window.alert("Welcome to Robot Gladiators!");
-  
+  //repeat and excute as long as the enemy-robot is alive
+  while(enemyHealth > 0){
     // ask player if they'd like to fight or run
     var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
   
@@ -62,8 +61,11 @@ var fight = function(enemyName) {
       window.alert("You need to pick a valid option. Try again!");
     }
   };
+  }
   
   for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50
+    fight(pickedEnemyName);
   }
   
